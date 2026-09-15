96ce60bd2e51142d12c35ed30e0b16f97c2e25b4
Rajesh Ananta Patil <139704615+Tejraj0319@users.noreply.github.com>
2026-09-15 23:03:34 +0530

    Time: 0 ms (100%), Space: 51.8 MB (98.59%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,12 +2,16 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    let string = s.trim().split(" ");
-    if(string.length === 0){
-        return 0
+var lengthOfLastWord = function (s) {
+    let str = s.trim()
+    let count = 0
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str[i] === " ") {
+            break
+        }
+        else {
+            count++;
+        }
     }
-    else{
-        return string[string.length-1].length;
-    }
+    return count;
 };
